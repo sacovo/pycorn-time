110a828f3c379affa9b9e1812ffc87dfc5257292
Sandro Covo <sandro@covo.ch>
2014-07-27 14:35:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         },
     include_package_data=True,
 
-    install_requires=['beautifulsoup4>=4.3.2'],
+    install_requires=['beautifulsoup4>=4.3.2', 'requests'],
 
     author="Sandro Covo",
     author_email="sandro@covo.ch",
